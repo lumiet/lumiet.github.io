613870508ddeec6fe8730248625890d4592ab68e
paig <31777095+lumiet@users.noreply.github.com>
2018-03-08 12:57:27 -0600

    Update spanish.js

--- spanish.js
+++ spanish.js
@@ -1,5 +1,7 @@
 var questionNumb = 3;
-var questionData = [{question : "To dance", answer: "bailar"}, {question: "To sing", answer: "cantar"}, {question: "To speak", answer: "hablar"}]
+var currQuestion = 0;
+var score = 0;
+var questionData = [{question : "to dance", answer: "bailar"}, {question: "to sing", answer: "cantar"}, {question: "to speak", answer: "hablar"}]
 
 function getRand(int x) {
 return Math.floor(Math.random()*x);
@@ -7,10 +9,23 @@ return Math.floor(Math.random()*x);
 
 
 function setQuestion() {
-var currQuestion = getRand(questionNumb);
-document.getElementById('phrase').content = questionData[currQuestion].question;
+currQuestion = getRand(questionNumb);
+document.getElementById('phrase').innerHTML = questionData[currQuestion].question;
+}
+
+function updateScore() {
+ document.getElementById('score').innerHTML = "Score: " + score;
 }
 
 function getData() {
-var userAnswer = document.getElementById('submission').value;
+var userAnswer = toLowerCase(document.getElementById('submission').value);
+  if(userAnswer==questionData[currQuestion].answer) {
+    document.getElementById('checkans').innerHTML = "Correct";
+    score++;
+    updateScore();
+    setQuestion();
+  }
+  else {
+   document.getElementById('checkans').innerHTML = "Incorrect, try again"; 
+  }
 }
