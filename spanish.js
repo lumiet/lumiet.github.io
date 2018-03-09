ef6b4fe8a72cc491114bc9161fbed69b62143a99
paig <31777095+lumiet@users.noreply.github.com>
2018-03-09 13:09:54 -0600

    Update spanish.js

--- spanish.js
+++ spanish.js
@@ -1,6 +1,28 @@
 var currQuestion = 0;
 var score = 0;
-var questionData = [{question:"to dance", answer:"bailar"},{question:"to speak", answer:"hablar"},{question:"to leave", answer:"salir"},{question:"to want",answer:"querer"},{question:"to have", answer:"tener"},{question:"to see", answer:"ver"},{question:"to make", answer:"hacer"},{question:"to ask", answer:"preguntar"}]
+var questionData = [
+            {question:"to dance", answer:"bailar"},
+            {question:"to speak", answer:"hablar"},
+            {question:"to leave", answer:"salir"},
+            {question:"to want",answer:"querer"},
+            {question:"to have", answer:"tener"},
+            {question:"to see", answer:"ver"},
+            {question:"to make", answer:"hacer"},
+            {question:"to ask", answer:"preguntar"},
+            {question:"to open", answer:"cubrir"},
+            {question:"to jump", answer:"saltar"},
+            {question:"to fly", answer:"voler"},
+            {question:"to like", answer:"gustar"},
+            {question:"to play", answer:"jugar"},
+            {question:"to save", answer:"ahorrar"},
+            {question:"to start", answer:"empezar"},
+            {question:"to die", answer:"morir"},
+            {question:"to live", answer:"vivir"},
+            {question:"to eat", answer:"comer"},
+            {question:"to wish for", answer:"pedir"},
+            {question:"to believe", answer:"creer"},
+            {question:"to know", answer:"saber"}
+]
 
 window.addEventListener('keydown', function (e) {
             var key = e.keyCode;
