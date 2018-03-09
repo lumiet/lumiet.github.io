154ea8beeac32aa2cc357e80eed2924a2af91ef4
paig <31777095+lumiet@users.noreply.github.com>
2018-03-08 22:54:34 -0600

    gfdgdfg

--- main.js
+++ main.js
@@ -22,6 +22,8 @@ function displayTime() {
 
 //constants
 {
+  var atkanitime = 0;
+  var click=false;
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   var enemy;
@@ -54,6 +56,7 @@ function attack(name, interval, pwr, radius, src, x, y) {
 	this.name = name;
 	this.img = new Image();
 	this.img.src = src;
+	//note: interval incremented by one every frame draw
 	this.interval = interval;
 	this.time = this.interval;
 	this.pwr = pwr;
@@ -61,11 +64,33 @@ function attack(name, interval, pwr, radius, src, x, y) {
 	this.ready = true;
 	this.x = x;
 	this.y = y;
-	this.visual = new component(100,100,"#212121",300,300);
+	this.buttnborder = new circle(x,y,37,"#ff3838");
+	this.buttnbase = new circle(x,y,35,"#212121");
+	this.buttn = new circle(x,y,35,"#616161");
 	this.checkready = function() {
-		if(this.interval <= (this.time/2*10)) this.ready = true;		
+		if(this.time >= this.interval) {
+			this.ready = true;	
+			this.time = this.interval;
+		}			
 		else this.ready = false;
+		var inc = (this.time/this.interval)*Math.PI;		
+		this.buttnbase.draw(0,2*Math.PI);
+		this.buttn.draw(.5*Math.PI-inc,.5*Math.PI+inc);			
+		ctx.drawImage(this.img, this.x-(.5*this.img.width),this.y-(.5*this.img.height));
 	}
+}
+
+function circle(x,y,r,color) {
+	this.x=x;
+	this.y=y;
+	this.r=r;
+	this.color=color;
+	this.draw = function(arc1,arc2) {
+		ctx.beginPath();
+		ctx.fillStyle = this.color;
+		ctx.arc(this.x,this.y,this.r,arc1,arc2);
+		ctx.fill();
+	}	
 }
 
 function character(species,speed,hp,alignment/*true if player, false if enemy*/) {
@@ -76,7 +101,6 @@ if(species==0){
 else {
 	this.img.src = "nymphous.png";
 }
-  this.atk = [{this : new attack("Bite", 1, 2, 25, "biteatk.png", 100,100)},{this : new attack("Bite", 1, 2, 15, "biteatk.png", 200,100)},{this : new attack("Bite", 1, 2, 15, "biteatk.png", 200,100)}];
   this.currentatk= 0;
   this.basespeed = speed; 
   this.currentspeed= speed;
@@ -86,17 +110,19 @@ else {
   this.headingleft = false;
   this.headingright = false; 
 if(alignment==true){
-	 this.y= canvas.height-160;	  
-     this.hpbar = new component(20, 100, "#23fc6f", canvas.width-110,canvas.height-100);
-  this.basehpbar = new component(20,300,"#212121",canvas.width-110,canvas.height-100);
+	this.atk = [new attack("Bite", 250, 2, 50, "bite.png", canvas.width-280,canvas.height-75),new attack("Bite", 600, 15, 50, "bite.png", canvas.width-200,canvas.height-75),new attack("Bite", 150, 2, 50, "bite.png", canvas.width-120,canvas.height-75)];
+	this.y = canvas.height-150;	  
+    this.hpbar = new component(20, 100, "#23fc6f", canvas.width-310,canvas.height-30);
+    this.basehpbar = new component(20,300,"#212121",canvas.width-310,canvas.height-30);
 }  
 else {
+	this.atk = [new attack("Bite", 400, 2, 25, "bite.png", 45,70),new attack("Bite", 20, 2, 15, "bite.png", 125,70),new attack("Bite", 20, 2, 15, "bite.png", 205,70)];
 	this.y= canvas.height*.4;	  
      this.hpbar = new component(20, 100, "#ff2323",10,10);
   this.basehpbar = new component(20,300,"#212121",10,10);
 }
   this.x= (canvas.width/2);
-
+  
   this.update = function(posneg) {	  
 		if(posneg>0&&this.movingright==true){	
 		  this.currentspeed += (this.currentspeed * this.acc);	
@@ -134,8 +160,7 @@ else {
   }
 }
 
-function component(height, width, color, x, y) {
-	this.gamearea = canvas;
+function component(height, width, color, x, y) {	
 	this.width = width;
 	this.x = x;
 	this.y = y;
@@ -147,9 +172,8 @@ function component(height, width, color, x, y) {
 	}
 	this.modify = function(width, height, color) {
 	this.width = width;
-	this.height = height;	
-	var context = canvas.getContext("2d");
-	context.fillStyle = color;
+	this.height = height;
+	ctx.fillStyle = color;
 	ctx.fillRect(x,y,width,height);
 	}	
 	
@@ -175,59 +199,118 @@ function ai(){
 }
 
 function aiatk() {
-if(enemy.atk[currentatk].this.ready == true) enemyatkx = getRndInteger(1,canvas.width);
+	if(enemy.atk[enemy.currentatk].ready) {		
+	enemyatkx = getRndInteger(1,canvas.width);		
+	enemy.atk[enemy.currentatk].time = 0;
+	}
 }
 
-function animate() {
-  ctx = clearGame(ctx);
-  if(lastclickx>= enemy.x-player.atk[player.currentatk].this.radius && lastclickx <= enemy.x+player.atk[player.currentatk].this.radius && lastclickx > 0) {
-	  lastclickx=0;
-	  if(player.hp>0) enemy.hp-= player.atk[player.currentatk].this.pwr;
-	  if(enemy.hp<0) enemy.hp=0;
+function checkhit(x,target,user) {
+	return ((x >= (target.x-user.atk[user.currentatk].radius)) && (x <= (target.x+user.atk[user.currentatk].radius)) && (x > 0));	
+	
+}
+
+function checkatk() {
+	if(checkhit(lastclickx,enemy,player) && player.atk[player.currentatk].ready && click) {
+	player.atk[player.currentatk].time = 0;
+	if(player.hp>0) enemy.hp-= player.atk[player.currentatk].pwr;
+	if(enemy.hp<0) enemy.hp=0;
+  }
+  else if(!checkhit(lastclickx,enemy,player) && player.atk[player.currentatk].ready && click){
+	player.atk[player.currentatk].time = 0;
   }
   
-  if(enemyatkx>= player.x-enemy.atk[enemy.currentatk].this.radius && enemyatkx <= player.x+enemy.atk[enemy.currentatk].this.radius && enemyatkx > 0) {
-	  enemyatkx=0;
-	  if(enemy.hp>0) player.hp-= enemy.atk[enemy.currentatk].this.pwr;
+  if(checkhit(enemyatkx, player, enemy) && enemy.atk[enemy.currentatk].ready) {	  
+	  if(enemy.hp>0) player.hp-= enemy.atk[enemy.currentatk].pwr;
 	  if(player.hp<0) player.hp=0;
   }
+}
+
+function updateatk() {
+	if(atkanitime>0)atkanitime-=.25;
+	for(var i=0;i<player.atk.length;i++) {
+		player.atk[i].time++;
+		if(i==player.currentatk) player.atk[i].buttnborder.draw(0,2*Math.PI);
+		player.atk[i].checkready();
+	}
+	
+	for(var i=0;i<enemy.atk.length;i++) {
+		enemy.atk[i].time++;
+		if(i==enemy.currentatk) enemy.atk[i].buttnborder.draw(0,2*Math.PI);
+		enemy.atk[i].checkready();
+	}
+}
+function checkinput() {	
   if (canvas.key && canvas.key == 37) {player.update(-1);}
   if (canvas.key && canvas.key == 39) {player.update(1);}
-  ctx.drawImage(bg.img, bg.x,bg.y);
-if(enemy.hp>0){
-  ctx.drawImage(enemy.img, enemy.x-(enemy.img.width/4), enemy.y, enemy.img.width*.6,enemy.img.height*.6);
+  if(canvas.key && canvas.key == 38 && player.currentatk > 0) {
+	  player.currentatk--;
+	  canvas.key=false;
+	  }
+  if(canvas.key && canvas.key == 40 && player.currentatk < player.atk.length-1) {
+	  player.currentatk++;
+	  canvas.key=false;
+	  }
 }
-if(player.hp>0){
-  ctx.drawImage(player.img, player.x-(player.img.width/4), player.y);
-  }
+function setcurratkcolor() {
+	for(var i=0;i<player.atk.length;i++) {
+		if(player.atk[i].ready) player.atk[i].buttn.color = "#edd976";
+		else player.atk[i].buttn.color = "#414141";
+	}
+	for(var i=0;i<enemy.atk.length;i++) {
+		if(enemy.atk[i].ready) enemy.atk[i].buttn.color = "#edd976";
+		else enemy.atk[i].buttn.color = "#414141";
+	}
+}
+/////////////////////////////////////////////
+function animate() {
+	ctx.globalAlpha=1;
+  ctx = clearGame(ctx);  
+  ctx.drawImage(bg.img, bg.x,bg.y);
+  checkinput();
+  setcurratkcolor();
+  if(enemy.hp>0)ctx.drawImage(enemy.img, enemy.x-(enemy.img.width/4), enemy.y, enemy.img.width*.6,enemy.img.height*.6);
+  if(player.hp>0) ctx.drawImage(player.img, player.x-(player.img.width/4), player.y);
+  
   enemy.basehpbar.modify(300,20,"#212121");
   player.basehpbar.modify(300,20,"#212121");
   enemy.hpbar.modify(300*(enemy.hp/enemy.basehp),20,"#ff2323");
   player.hpbar.modify(300*(player.hp/player.basehp),20,"#23fc6f");
-  ctx.fillRect(enemy.x-25, 200, 50,10);
   ctx.fillStyle = "white";
   ctx.fillText(enemy.hp + "/" + enemy.basehp,15,24);
-  ctx.fillText(player.hp + "/" + player.basehp,335,canvas.height-85);
+  ctx.fillText(player.hp + "/" + player.basehp,canvas.width-305,canvas.height-15);
+  updateatk();
+  checkatk();  
+  var img = new Image();
+  img.src="hit.png";
+  ctx.globalAlpha = (atkanitime/10);
+  ctx.drawImage(img,lastclickx-img.width/2,enemy.y+20);
 	
 }
 
 function draw() {	
+	
 	ctx.font = "12px Arial";
    window.addEventListener('keydown', function (e) {
             canvas.key = e.keyCode;
         });
-        window.addEventListener('keyup', function (e) {
+   window.addEventListener('keyup', function (e) {
             canvas.key = false;
   });
 canvas.addEventListener('mousedown', function(e) {
-    var rect = canvas.getBoundingClientRect();
+    if(player.atk[player.currentatk].ready){
+	atkanitime = 10;
+	var rect = canvas.getBoundingClientRect();
 	lastclickx = e.clientX - rect.left;
 	lastclicky = Math.ceil(e.clientY - rect.top);
+	}
+	click=true;
 }, false);
 canvas.addEventListener('mouseup', function(e) {
-	lastclickx = 0;
-	lastclicky = 0;
-}, false);
+	// lastclickx = 0;
+	// lastclicky = 0;
+	click=false;
+	}, false);
 
 player = new character(0,7,50,1);
 enemy = new character(1,5,50,0);
@@ -240,20 +323,3 @@ enemy = new character(1,5,50,0);
   bg.img.src = "bg.png";
  
 }
-/*
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-*/
