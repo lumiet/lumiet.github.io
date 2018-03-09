1c1474149fd18ffcb648e193a200a0cac43e4402
paig <31777095+lumiet@users.noreply.github.com>
2018-03-09 12:50:59 -0600

    Update spanish.js

--- spanish.js
+++ spanish.js
@@ -1,7 +1,11 @@
-var questionNumb = 3;
 var currQuestion = 0;
 var score = 0;
-var questionData = [{question : "to dance", answer: "bailar"}, {question: "to sing", answer: "cantar"}, {question: "to speak", answer: "hablar"}];
+var questionData = [{question:"to dance", answer:"bailar"},{question:"to speak", answer:"hablar"},{question:"to leave", answer:"salir"},{question:"to want",answer:"querer"},{question:"to have", answer:"tener"},{question:"to see", answer:"ver"},{question:"to make", answer:"hacer"},{question:"to ask", answer:"preguntar"}]
+
+window.addEventListener('keydown', function (e) {
+            var key = e.keyCode;
+if(key==13) getData();
+        });
 
 
 function getRand(x) {
@@ -10,7 +14,7 @@ return Math.floor(Math.random()*x);
 
 
 function setQuestion() {
-currQuestion = getRand(questionNumb);
+currQuestion = getRand(questionData.length);
 document.getElementById('phrase').innerHTML = questionData[currQuestion].question;
 }
 
@@ -19,14 +23,13 @@ function updateScore() {
 }
 
 function getData() {
- alert("got here");
 var userAnswer = document.getElementById('submission').value.toLowerCase();
- alert(userAnswer);
   if(userAnswer==questionData[currQuestion].answer) {
     document.getElementById('checkans').innerHTML = "Correct";
     score++;
     updateScore();
     setQuestion();
+document.getElementById('submission').value = "";
   }
   else {
    document.getElementById('checkans').innerHTML = "Incorrect, try again"; 
