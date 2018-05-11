0b12d866e433c9d4637f920cb8e320db6d336bf3
paig <31777095+lumiet@users.noreply.github.com>
2018-05-11 14:21:47 -0500

    Update minesweep.js

--- minesweep.js
+++ minesweep.js
@@ -6,7 +6,7 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-
+document.addEventListener('dblclick', function() { event.preventDefault() });
 
 
 function setUp() {
