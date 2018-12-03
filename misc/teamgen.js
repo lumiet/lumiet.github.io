6f16d66b43e9fd8fcd3ab7440d1f8194d8e359fd
paig <31777095+lumiet@users.noreply.github.com>
2018-12-02 20:53:16 -0600

    Update teamgen.js

--- misc/teamgen.js
+++ misc/teamgen.js
@@ -1136,7 +1136,7 @@ var pokemon = [ "Venusaur",
 		else { content+= "and " + partyname[i] + "."; }
 	}
 for(var z = 1; z<=6; z++) {	  
-	document.getElementById(z).innerHTML = "<img src='https://img.pokemondb.net/sprites/x-y/normal/" + partyname[z-1].toLowerCase() + ".png'>";	
+	document.getElementById(z).innerHTML = "<img src='https://img.pokemondb.net/sprites/xydex/" + partyname[z-1].toLowerCase() + ".png'>";	
 }
 document.getElementById("main").innerHTML = content;
 	return;
