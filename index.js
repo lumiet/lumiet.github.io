99797ce741e86f1d4b3b4c3dadbafcb4103a4aa4
paig <31777095+lumiet@users.noreply.github.com>
2018-12-11 10:40:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var img = document.getElementById('zekrom');
 var x = 0;
 var y = 0;
-var amp = 50;
+var amp = 25;
 
 function main() {
 img.style.position = 'absolute';
@@ -9,7 +9,7 @@ setInterval(move, 20);
 }
 
 function move() {
-x+=5;
+x+=1;
 y=Math.sin(x)*amp;
 img.style.left = x+"px";
 img.style.top = y+"px";
