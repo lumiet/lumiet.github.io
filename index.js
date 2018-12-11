18bff85089a5004c0d84ff7cc5ededc4f6c665e8
paig <31777095+lumiet@users.noreply.github.com>
2018-12-11 10:03:23 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var img = document.getElementById(zekrom);
+var x = 0;
+var y = 0;
+var amp = 50;
+
+function main() {
+img.style.position = 'absolute';
+setInterval(move, 20);
+}
+
+function move() {
+x+=5;
+y=Math.sin(x)*amp;
+img.style.left = "\(x) px";
+img.style.top = "\(y) px";
+}
