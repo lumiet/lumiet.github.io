224d614ff8f03a0a683b8f5303faa4a59a1b2194
lumiet <31777095+lumiet@users.noreply.github.com>
2021-04-28 21:57:45 -0500

    sdfgfd

--- test/main.js
+++ test/main.js
@@ -7,5 +7,7 @@ var ctx = document.getElementById('img').getContext('2d');
 
 baseimg.onload = function() {
 	ctx.drawImage(baseimg,0,0);
+	ctx.fillStyle="#000000";
+	ctx.fillRect(0,0,150,150);
 }
 }
