b243feb420bf43bee48263503b5d5f242346efca
lumiet <31777095+lumiet@users.noreply.github.com>
2021-05-01 03:47:06 -0500

    cgvbgfvb

--- test/main.js
+++ test/main.js
@@ -14,14 +14,17 @@ for(var i = 0; i<3; i++){
 	ctx.filter = 'hue-rotate('+ (Math.random()*360) +'deg)';	
 	console.log(ctx.filter);
 	switch(i) {
-		case 0: base.onload = function() {this.src = "assets/base.png"; ctx.drawImage(this,0,0);console.log("kdfjgf");}; newBase.src = canvas.toDataURL(); console.log(newBase.src); break;
-		case 1: darkBase.onload = function() {this.src = "assets/dark-base.png"; ctx.drawImage(this,0,0);}; newDarkBase.src = canvas.toDataURL(); console.log(newDarkBase.src); break;
-		case 2: lightBase.onload = function() {this.src = "assets/light-base.png"; ctx.drawImage(this,0,0);}; newLightBase.src = canvas.toDataURL(); console.log(newLightBase.src); break;	
+		case 0: base.onload = function() { ctx.drawImage(this,0,0);console.log("kdfjgf");}; newBase.src = canvas.toDataURL(); console.log(newBase.src); break;
+		case 1: darkBase.onload = function() {ctx.drawImage(this,0,0);}; newDarkBase.src = canvas.toDataURL(); console.log(newDarkBase.src); break;
+		case 2: lightBase.onload = function() {ctx.drawImage(this,0,0);}; newLightBase.src = canvas.toDataURL(); console.log(newLightBase.src); break;	
 		default: break;
 	}
 	console.log(i);
 	//ctx.clearRect(0,0,canvas.width,canvas.height);
 } 
+base.src = "assets/base.png";
+darkBase.src = "assets/dark-base.png"; 
+lightBase.src = "assets/light-base.png"; 
 
 console.log("lol hi");
 var testElem = document.getElementById("test");
