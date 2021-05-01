4b7806f7226ffaa4ed3e9b603bf0ed805f4922d8
lumiet <31777095+lumiet@users.noreply.github.com>
2021-05-01 03:38:22 -0500

    fdsgsfdg

--- test/main.js
+++ test/main.js
@@ -3,18 +3,20 @@ var ctx = canvas.getContext('2d');
 
 function main() {
 var base = new Image();
+var newBase = new Image();
 var darkBase = new Image();
-console.log(darkBase.src);
+var newDarkBase = new Image();
 var lightBase = new Image();
+var newLightBase = new Image();
 
 
 for(var i = 0; i<3; i++){	
 	ctx.filter = 'hue-rotate('+ (Math.random()*360) +'deg)';	
 	console.log(ctx.filter);
 	switch(i) {
-		case 0: base.onload = function() {this.src = "assets/base.png"; ctx.drawImage(this,0,0);console.log("kdfjgf");}; base.src = canvas.toDataURL(); console.log(base.src); break;
-		case 1: darkBase.onload = function() {this.src = "assets/dark-base.png"; ctx.drawImage(this,0,0);}; darkBase.src = canvas.toDataURL(); console.log(darkBase.src); break;
-		case 2: lightBase.onload = function() {this.src = "assets/light-base.png"; ctx.drawImage(this,0,0);}; lightBase.src = canvas.toDataURL(); console.log(lightBase.src); break;	
+		case 0: base.onload = function() {this.src = "assets/base.png"; ctx.drawImage(this,0,0);console.log("kdfjgf");}; newBase.src = canvas.toDataURL(); console.log(newBase.src); break;
+		case 1: darkBase.onload = function() {this.src = "assets/dark-base.png"; ctx.drawImage(this,0,0);}; newDarkBase.src = canvas.toDataURL(); console.log(newDarkBase.src); break;
+		case 2: lightBase.onload = function() {this.src = "assets/light-base.png"; ctx.drawImage(this,0,0);}; newLightBase.src = canvas.toDataURL(); console.log(newLightBase.src); break;	
 		default: break;
 	}
 	console.log(i);
@@ -23,7 +25,7 @@ for(var i = 0; i<3; i++){
 
 console.log("lol hi");
 var testElem = document.getElementById("test");
-test.style = 'background: url("'+base.src+'");';
+test.style = 'background: url("'+newBase.src+'");';
 
 
 /*	console.log("losdfsdf");
