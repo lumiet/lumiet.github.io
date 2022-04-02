85b49b829426b737a65545757d7f65d117d5e4fd
lumiet <31777095+lumiet@users.noreply.github.com>
2022-04-02 00:19:57 -0500

    asdfsdg

--- misc/melancalc/main.js
+++ misc/melancalc/main.js
@@ -9,11 +9,14 @@ function main() {
 		document.getElementById('z').addEventListener('input', updateValues);
 		document.getElementById('potd').addEventListener('input', updateValues);
 		document.getElementById('eggshatched').addEventListener('input', updateValues);
+		
+		document.getElementById("lvl").innerHTML = document.getElementById("basealbino").value;
 }
 
 function updateValues() {
 	var baseShiny = document.getElementById("baseshiny").value;	
-	var baseAlbino = document.getElementById("basealbino").value;
+	var albIndex = document.getElementById("basealbino").value;
+	document.getElementById("lvl").innerHTML = document.getElementById("basealbino").value;
 	var sei = document.getElementById("sei").value;	
 	var longChain = document.getElementById("longchain").value;	
 	var eggsHatched = document.getElementById("eggshatched").value;	
@@ -22,26 +25,31 @@ function updateValues() {
 	var z = document.getElementById("z").checked;
 	var potd = document.getElementById("potd").checked;
 	longChain = 1 + longChain/100;
-	calculateOdds(baseShiny,baseAlbino,sei,longChain,shinyCharm,uberCharm,z,potd,eggsHatched);
+	calculateOdds(baseShiny,albIndex,sei,longChain,shinyCharm,uberCharm,z,potd,eggsHatched);
 }
 
-function calculateOdds(baseShiny,baseAlbino,sei,longChain,shinyCharm,uberCharm,z,potd,eggsHatched) {
+function calculateOdds(baseShiny,albIndex,sei,longChain,shinyCharm,uberCharm,z,potd,eggsHatched) {
 	/*
 	If your Shiny chances are 1/X, and Sei power is S, then first convert Sei power into a modifier P = 50-S 
 	and then your Sei-boosted Shiny chances are 1/(sqrt(X/P)*P) - 
 	which now that I'm looking at it is really just 1/(sqrt(X) * sqrt(50-S))
 	*/
+	var baseAlbino = 6145.2;
+	const albBoost = [1,2,4,8,12.8,22.76,34.14];
+	console.log("Albino odds at level " + albIndex + " (before Z): 1/" + baseAlbino/albBoost[albIndex-1]);
+	baseAlbino /= albBoost[albIndex-1]; //applies boost from alb radar level
+	
 	
 	if(shinyCharm) baseShiny/=2.5; //2.5x shiny odds
 	if(potd) baseShiny /= 1.1; //10% shiny boost
 	if(sei>0) baseShiny = Math.sqrt(baseShiny) * Math.sqrt(50-sei); //apply sei boost formula. must be calculated in this order
-	
+	console.log("Shiny odds after boosts: 1/" + baseShiny); 
 	if(z) baseAlbino /=1.5; //1.5x albino odds (+50%)
 		
 	var result = baseShiny * baseAlbino; //base melan odds
 	if(uberCharm) result/=6; //6x melan odds
 	if(longChain!=0) result/=longChain;
-	console.log(result);
+	console.log("Calculated melan odds: 1/" + result);
 	document.getElementById("results").innerHTML = "Calculated odds: 1/" + Number.parseFloat(result.toFixed(3)) + ", or " + Number.parseFloat((1/result*100).toFixed(3)) + "%";
 	document.getElementById("hatchedresults").innerHTML = "Odds of hatching at least one Shiny: " + Number.parseFloat(((1-Math.pow((baseShiny-1)/baseShiny,eggsHatched))*100).toFixed(3))+"%";
 	document.getElementById("hatchedresults").innerHTML += "<br>Odds of hatching at least one Albino: " + Number.parseFloat(((1-Math.pow((baseAlbino-1)/baseAlbino,eggsHatched))*100).toFixed(3))+"%";
