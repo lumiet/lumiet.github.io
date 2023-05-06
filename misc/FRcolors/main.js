e1c1521c173c0afeca84fd424dfb0c065c4378d5
lumiet <31777095+lumiet@users.noreply.github.com>
2023-05-06 01:25:10 -0500

    f

--- misc/FRcolors/main.js
+++ misc/FRcolors/main.js
@@ -2,6 +2,17 @@ function main() {
 	
 const rules = window.document.styleSheets[0].cssRules;
 for(var i = 0; i < rules.length; i++) {
-console.log(rules[i].selectorText);
+var colorName = rules[i].selectorText.split('-')[1];
+var colorValue = rules[i].style.background;
+var textColor = rules[i].style.color;
+const newCanvas = document.body.appendChild(document.createElement("canvas"));
+newCanvas.width = 100;
+newCanvas.height = 100;
+var ctx = newCanvas.getContext("2d");
+ctx.fillStyle = colorValue;
+ctx.fillRect(0,0,newCanvas.width,newCanvas.height);
+ctx.fillStyle = textColor;
+ctx.font = "12px serif";
+ctx.fillText(colorName, 5, newCanvas.height - 20);
 }
 }
