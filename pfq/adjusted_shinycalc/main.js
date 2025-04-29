5aa364e9dae1763c1cd22f571d4caab588329965
lance <31777095+lumiet@users.noreply.github.com>
2025-04-29 13:18:21 -0500

    very heavy wip

---
+++ pfq/adjusted_shinycalc/main.js
@@ -0,0 +1,112 @@
+function main() {
+	updateValues();
+		document.getElementById('hypermode').addEventListener('change', updateValues);
+		document.getElementById('basealbino').addEventListener('change', updateValues);
+		document.getElementById('sei').addEventListener('change', updateValues);
+		document.getElementById('longchain').addEventListener('change', updateValues);
+		document.getElementById('shinycharm').addEventListener('input', updateValues);
+		document.getElementById('ubercharm').addEventListener('input', updateValues);
+		document.getElementById('z').addEventListener('input', updateValues);
+		document.getElementById('typerace').addEventListener('input', updateValues);
+		document.getElementById('goldamulet').addEventListener('input', updateValues);
+		document.getElementById('silveramulet').addEventListener('input', updateValues);
+		document.getElementById('potd').addEventListener('input', updateValues);
+		document.getElementById('eggcount').addEventListener('change', updateValues);
+		document.getElementById('minusEgg').addEventListener('click', (e) => {
+			document.getElementById('eggcount').value = parseInt(document.getElementById('eggcount').value) - 1;
+			updateValues();
+		});
+		document.getElementById('plusEgg').addEventListener('click', (e) => {
+			document.getElementById('eggcount').value = parseInt(document.getElementById('eggcount').value) + 1;
+			updateValues();
+		});
+}
+function updateValues() {
+	var hypermode = document.getElementById("hypermode").checked;	
+	var albIndex = document.getElementById("basealbino").value;
+	document.getElementById("lvl").innerHTML = "Albino radar level: " + document.getElementById("basealbino").value;
+	var sei = parseInt(document.getElementById("sei").value);	
+	var longChain = document.getElementById("longchain").value;
+	var goldAmulet = document.getElementById("goldamulet").checked;
+	var silverAmulet = document.getElementById("silveramulet").checked;
+	var shinyCharm = document.getElementById("shinycharm").checked;
+	var uberCharm = document.getElementById("ubercharm").checked;
+	var z = document.getElementById("z").checked;	
+	var typerace = document.getElementById("typerace").checked;
+	var potd = document.getElementById("potd").checked;
+	var eggcount = document.getElementById("eggcount").value;
+	document.getElementById("count").innerHTML = eggcount + " Eggs since last Shiny";
+	calculateOdds(hypermode,albIndex,sei,longChain,shinyCharm,uberCharm,z,typerace,potd,silverAmulet,goldAmulet,eggcount);
+}
+
+function calculateOdds(hypermode,albIndex,sei,longChain,shinyCharm,uberCharm,z,typerace,potd,silverAmulet,goldAmulet,eggcount) {
+	var txt = document.getElementById("shinyexplanation");
+	txt.innerHTML = "";
+	var baseOdds = 1000; 
+	txt.innerHTML += "<li>Base odds: 1/" + baseOdds + "</li>";
+	if(hypermode) {
+		baseOdds /= 2;
+		txt.innerHTML += "<li>Hypermode (2x bonus) reduces base odds to 1/" + format(baseOdds) + "</li>";
+	}
+	if(goldAmulet) {
+		baseOdds /= 1.5;
+		txt.innerHTML += "<li>Gold Amulet (1.5x bonus) reduces base odds to 1/" + format(baseOdds) + "</li>";
+	};
+	if(shinyCharm) {		
+		baseOdds /= 2.5;
+		txt.innerHTML += "<li>Shiny Charm (2.5x bonus) reduces base odds to 1/" + format(baseOdds) + "</li>";
+	}
+	if(potd) {
+		baseOdds /= 2;
+		txt.innerHTML += "<li>PotD (2x bonus) reduces base odds to 1/" + format(baseOdds) + "</li>";
+	}
+	var decay = .99;
+	if(sei) {
+		decay = .99 - sei/25 * 0.01;
+		txt.innerHTML += "<li>"+ sei + " Sei Power means decay is " + decay + " (down from 0.99 on non-Sei days)</li>" 
+	}
+	txt.innerHTML += "<li>Final base odds: 1/" + Math.ceil(baseOdds) + "</li>";
+	var odds = [];
+	
+	for(var i=0; i<500; i++) {
+		if(i==0) odds.push(baseOdds);
+		else odds.push((odds[i-1]-1) * decay + 1);
+	}
+	odds.push(1);
+	console.log(odds);
+	txt.innerHTML += "If there have been " + eggcount + " egg(s) since your last Shiny, the odds that this egg will be a Shiny are <b>1/" + Math.ceil(odds[eggcount]) + "</b>."; 
+	
+	
+	const albBoost = [6144, 3072, 1536, 768, 480, 270, 180];
+	baseAlbino = albBoost[albIndex-1];
+	
+	txt = document.getElementById("melanexplanation");
+	txt.innerHTML = "";
+	txt.innerHTML += "<li>Albino odds at level "+ albIndex + ": 1/" + format(baseAlbino) + "</li>"; 
+	if(silverAmulet) {
+		baseAlbino /= (4/3);
+		txt.innerHTML += "<li>Silver Amulet (1.33x bonus) reduces base odds to 1/" + format(baseAlbino) + "</li>";
+	}
+	if(z) {
+		baseAlbino /= 1.5;
+		txt.innerHTML += "<li>Z-Crystal (1.5x bonus) reduces base odds to 1/" + format(baseAlbino) + "</li>";
+	}
+	if(typerace) {
+		baseAlbino/=1.2;
+		txt.innerHTML += "<li>Type Race (1.2x bonus) reduces base odds to 1/" + format(baseAlbino) + "</li>";
+	}
+	if(uberCharm) {
+		baseAlbino /= 6;
+		txt.innerHTML += "<li>Ubercharm (6x bonus) reduces base odds to 1/" + format(baseAlbino) + "</li>";
+	}
+	if(longChain) {
+		baseAlbino /= (1+longChain*.01);
+		txt.innerHTML += "<li>Cobalt Amulet (Long Chain Bonus) ("+longChain+"%) reduces base odds to 1/" + format(baseAlbino) + "</li>";
+	}
+	txt.innerHTML += "Final odds that this Shiny will become a Melanistic: <b>1/" + Math.ceil(baseAlbino) + "</b>."
+	+ "<br>This means the overall odds that this egg will become a Melanistic (Shiny * Albino * Melan modifiers) is <b>1/" + (Math.ceil(baseAlbino) * Math.ceil(odds[eggcount])) + "</b>.";
+}
+
+function format(n) {
+	return Number.parseFloat((n).toFixed(3));
+}
